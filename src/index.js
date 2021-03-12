516608a047fc156845f6e76815687a31eaa7f0c6
fraterpavlo <shukan1999@mail.ru>
2021-03-12 19:47:50 +0300

    comleted

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (matrix === undefined){return [];} 
+    else if (matrix.length == 0) {return []}
+    else {
+        for (let i = 1; i < matrix.length; i +=2) {
+            function compareNumbers(a, b) {
+                return b - a;};
+            matrix[i] = matrix[i].sort(compareNumbers);
+        };
+        let newMatrix = matrix.flat(Infinity);
+        return newMatrix;
+    }
+};
